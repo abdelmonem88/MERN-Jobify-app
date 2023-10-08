66eb913efb6701424e6083b95c3b3a19c7091fbb
abdelmonem88 <abdelmonem1888@gmail.com>
2023-10-09 00:46:08 +0300

    initial server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,15 @@ import express, { Request, Response, NextFunction } from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 // register routes
 app.get("/", (req, res) => res.send("Hello World!"));
+app.post("/", (req, res) => {
+  console.log(req);
+
+  res.json({ message: "Data received", data: req.body });
+});
 
 // start express server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
